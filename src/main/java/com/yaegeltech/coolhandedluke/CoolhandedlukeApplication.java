fba2f3d140fb7dbeb9e3d45c812dc5db3f2dd5c9
Luke Yaegel <prodeveloper77@gmail.com>
2019-12-05 21:20:44 -0500

    First Commit

---
+++ src/main/java/com/yaegeltech/coolhandedluke/CoolhandedlukeApplication.java
@@ -0,0 +1,13 @@
+package com.yaegeltech.coolhandedluke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolhandedlukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolhandedlukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaegeltech/coolhandedluke/CoolhandedlukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaegeltech.coolhandedluke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolhandedlukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
